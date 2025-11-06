57cc60f7b8f398486e12e7c40030c6b15525c8fa
Carter Irish <cdi9919@rit.edu>
2025-11-06 17:46:11 -0500

    Added some potential function signatures to the string.js util. Needs implementation.

---
+++ string.js
@@ -0,0 +1,15 @@
+export function validEmail(email) { };
+
+export function validPhoneNumber(phoneNumber) { };
+
+export function capitalizeFirstLetter(string) { };
+
+export function truncateString(string, maxLength) { };
+
+export function sanitizeString(string) { };
+
+export function generateRGBColor() { };
+
+export function stringToArray() { };
+
+export function arrayToString() { };
